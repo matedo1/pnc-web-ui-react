a84ec9c4079355fef6d7fb8a7e77d49406a656ae
Martin Kelnar <mkelnar@redhat.com>
2026-04-29 09:42:53 +0000

    Decommission legacy content

--- src/components/DashboardWidget/__tests__/DashboardWidget.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import { DashboardWidget } from 'components/DashboardWidget/DashboardWidget';
-
-test('renders Dashboard Widget', () => {
-  render(<DashboardWidget title="TEST TITLE" src="https://localhost/" />);
-  expect(screen.getByText('TEST TITLE')).toBeInTheDocument();
-});
